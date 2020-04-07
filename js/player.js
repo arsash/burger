2fe888ad0c68b286714f6f0db95a3cb1be18cfdf
Arshaluys Aslanyan <a.aslanyan@inbox.ru>
2020-04-07 03:13:05 +0300

    player design done

--- js/player.js
+++ js/player.js
@@ -1,4 +1,48 @@
 let player;
+let playerContainer = $('.player__wrap');
+
+let eventsInit = () => {
+  $('.play').on('click', (e) => {
+    const btn = $(e.currentTarget);
+    if(playerContainer.hasClass('paused')) {
+        playerContainer.removeClass('paused');
+        player.pauseVideo();
+    }else {
+      playerContainer.addClass('paused');
+      player.playVideo();
+    } 
+  })
+};
+
+// $('duration__completed').text.player.getCurrentTime();
+
+const formatTime = timeSec => {
+  const roundSec = Math.round(timeSec);
+  const minutes = addZero(Math.floor(roundSec / 60));
+  const seconds = addZero(roundSec - minutes * 60);
+
+  function addZero(num) {
+    return num < 10 ? `0${num}` : num;
+  }
+
+  return `${minutes}:${seconds}`;
+}
+
+const onPlayerReady = () => {
+  let interval;
+  const durationSec = player.getDuration();
+
+  $('.duration__estimated').text(formatTime(durationSec));
+
+  if(typeof interval === 'undefined'){
+    clearInterval(interval);
+  }
+
+  interval = setInterval(function(){
+    const completedSec = player.getCurrentTime();
+    $('.duration__completed').text(formatTime(completedSec));
+  }, 1000)
+};
 
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
@@ -6,7 +50,7 @@ function onYouTubeIframeAPIReady() {
     width: '100%',
     videoId: 'oqeW9YMHweo',
     events: {
-      // 'onReady': onPlayerReady,
+      'onReady': onPlayerReady,
       // 'onStateChange': onPlayerStateChange
     },
     playerVars: {
@@ -19,3 +63,5 @@ function onYouTubeIframeAPIReady() {
     }
   })
 };
+
+eventsInit();
