f734836632d1ebecfd7e7a892a52c0de5e798a05
Arshaluys Aslanyan <a.aslanyan@inbox.ru>
2020-04-12 20:10:15 +0300

    form code review

--- js/form.js
+++ js/form.js
@@ -1,20 +1,21 @@
-const orderForm = document.querySelector('.form__inputs');
+const orderForm = document.querySelector('#myForm');
 const sendBtn = document.querySelector('.order__btn');
 const formServer = 'https://webdev-api.loftschool.com/sendmail';
 
-sendBtn.addEventListener('click', function (e) {
+orderForm.addEventListener('submit', function (e) {
     e.preventDefault();
 
     if(validateForm(orderForm)){
 
-        const data = {
-            name: orderForm.elements.name.value,
-            phone: orderForm.elements.phone.value,
-            comment: orderForm.elements.comment.value
-        }
+        const data = new FormData();
+              data.append('name', orderForm.elements.name.value);
+              data.append('phone', orderForm.elements.phone.value);
+              data.append('comment', orderForm.elements.comment.value);
+              data.append('to', 'a.aslanyan2020@gmail.com');
+        
         const xhr = new XMLHttpRequest();
         xhr.open('POST', formServer);
-        xhr.send(JSON.stringify(data));
+        xhr.send(data);
         xhr.addEventListener('load', () => {
             if(xhr.response.status){
                 $('.modal').css({
