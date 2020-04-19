b186e67970da752fd1f5915239317c82950734dc
Arshaluys Aslanyan <a.aslanyan@inbox.ru>
2020-04-19 14:40:30 +0300

    review modal is done

--- js/review.js
+++ js/review.js
@@ -1,9 +1,27 @@
 $(document).ready(function(){
 
-    new jBox('Modal', {
-        attach: '#myModal',
-        title: 'Hurray!',
-        content: 'This is my modal window'
-      });
+  const revBtn = $('.review__detail-link');
+
+  revBtn.on('click', (e) => {
+    e.preventDefault();
+    const btnParent = e.target.parentNode;
+    const revText = btnParent.previousElementSibling;
+    const revName = revText.previousElementSibling;
+    
+    $('.modal__review').css({
+      'display': 'flex',
+      'color': 'white'
+    })
+
+    $('.modal__text-head').text(revName.innerText);
+    $('.modal__text-content').text(revText.innerText);
+
+  });
+
+  $('.modal__btn').on('click', () => {
+    $('.modal__review').css({
+      'display': 'none'
+    })
+  });
 
 });
